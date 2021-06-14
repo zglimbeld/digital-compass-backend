7fe4c9fe61ec591b5d2dc378b90a614b08b2ac5e
zglimbeld <zglimbeld@gmail.com>
2021-06-14 21:53:48 +0300

    feat: add token exchange and media query

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express();
 const fetch = require('node-fetch');
 
 app.get('/', (req, res) => {
-  // const param = req.query.param;
-
-  res.json({ 'Hello': 'World!' });
+  res.redirect(`https://api.instagram.com/oauth/authorize?client_id=469218404399973&redirect_uri=http://digitalcompass.azurewebsites.net/landing&scope=user_profile,user_media&response_type=code`);
 });
 
 app.get('/landing', (req, res) => {
   const codeParam = req.query.code;
-
   const code = codeParam.slice(0, -2);
 
   console.log(`Received code ${code}`);
@@ -19,7 +16,7 @@ app.get('/landing', (req, res) => {
     client_id: '469218404399973',
     client_secret: '50976c32493171dfd63e20358bfeaed9',
     grant_type: 'authorization_code',
-    redirect_uri: 'https://zglimbeld.github.io/test-dc/',
+    redirect_uri: 'http://digitalcompass.azurewebsites.net/landing',
     code: code
   }
 
@@ -31,7 +28,18 @@ app.get('/landing', (req, res) => {
     headers: { 'Content-Type': 'application/json' }
   })
   .then(res => res.json())
-  .then(json => console.log(json));
+  .then(json => {
+    console.log(json);
+    const access_token = json.access_token;
+  });
+});
+
+app.get('/media', (req, res) => {
+  fetch(`https://graph.instagram.com/me/media?field=id,caption&access_token=${access_token}`, {
+    method: 'GET'
+  })
+  .then(res => res.json())
+  .then(json => res.json(json))
 });
 
 const port = process.env.PORT || 8080
